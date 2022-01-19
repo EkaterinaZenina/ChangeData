8e45a370fb55e5dd05301671c83433aa7a055987
Екатерина Зенина <zeninaekaterin@gmail.com>
2022-01-19 13:10:28 +0300

    update

--- src/test/java/ShouldRegByAccountTest.java
+++ src/test/java/ShouldRegByAccountTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
@@ -10,7 +11,6 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class ShouldRegByAccountTest {
     String planingDate = DataGenerator.generateDate(3);
-    String newPlaningDate = DataGenerator.generateDate(5);
     @Test
     public void shouldRegByAccount() {
         open("http://localhost:9999");
@@ -21,13 +21,7 @@ public class ShouldRegByAccountTest {
         $("[data-test-id='name'] .input__control").setValue(DataGenerator.getName());
         $x("//*[@name='phone']").setValue(DataGenerator.getPhone());
         $(".checkbox__box").click();
-        $(".button__text").click();
         $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на "+ planingDate), Duration.ofSeconds(15)).shouldBe(visible);
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(DataGenerator.newPlanningDate(1));
-        $$(".button").find(exactText("Запланировать")).click();
-        $$(".button__text").find(exactText("Перепланировать")).click();
-        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + newPlaningDate) , Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
 
